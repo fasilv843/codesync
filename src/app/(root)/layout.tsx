c459bbabd2989f0b3f09ff6be8830f9bf36d5866
Fasil <fasilvaliyattil843@gmail.com>
2025-04-10 21:32:27 +0530

    added camera and mic to meeting

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import StreamClientProvider from '@/components/providers/StreamClientProvider'
+import React from 'react'
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <StreamClientProvider>
+        {children}
+    </StreamClientProvider>
+  )
+}
+
+export default Layout
--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,6 +1,40 @@
+"use client"
+
+import LoaderUI from "@/components/LoaderUI";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useParams } from "next/navigation"
+import { useState } from "react";
+
 function MeetingPage() {
+    const { id } = useParams();
+    const { isLoaded } = useUser();
+
+    const { call, isCallLoading } = useGetCallById(id);
+    const [isSetupCompleted, setIsSetupCompleted] = useState(false);
+    if (!isLoaded || isCallLoading) return <LoaderUI />
+
+    if (!call) {
+        return (
+          <div className="h-screen flex items-center justify-center">
+            <p className="text-2xl font-semibold">Meeting not found</p>
+          </div>
+        );
+    }
+
   return (
-    <div>MeetingPage</div>
+    <StreamCall call={call}>
+        <StreamTheme>
+            {!isSetupCompleted ? (
+                <MeetingSetup onSetupComplete={() => setIsSetupCompleted(true)} />
+            ) : (
+                <MeetingRoom />
+            )}
+        </StreamTheme>
+    </StreamCall>
   )
 }
 
